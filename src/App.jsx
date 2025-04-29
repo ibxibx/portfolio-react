9b0838f968d208cdbc59afe119e1c1e67c16f3dd
Ian Baumeister <magnyt@gmail.com>
2025-04-29 23:20:04 +0200

    Changed the Router for HashRouter for correct routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./components/layout/Layout";
 import Home from "./pages/Home";
 import About from "./pages/About";
@@ -20,7 +20,7 @@ const App = () => {
   }, []);
 
   return (
-    <Router basename="/">
+    <Router>
       <Layout isLoading={isLoading}>
         <Routes>
           <Route path="/" element={<Home />} />
--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -123,6 +123,7 @@ const ProjectBox = ({
     </motion.div>
   );
 };
+
 const Work = () => {
   const projects = [
     {
